7a8c7258c786ba117f416ca21dab2a9063e95c18
aosenlucky <senlucky0217@gmail.com>
2026-06-13 15:30:29 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+// 💡 修复：全局引入图标库
+import '@phosphor-icons/web/regular'
+import '@phosphor-icons/web/fill'
 import App from './App.vue'
 import router from './router'
 
